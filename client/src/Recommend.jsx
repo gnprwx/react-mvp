119541852f3e96e8d068663b917df53130523376
Greg Pappas <gnpappas@proton.me>
2023-11-17 17:58:37 -1000

    Prototype styling

--- client/src/Recommend.jsx
+++ client/src/Recommend.jsx
@@ -1,5 +1,5 @@
 const Recommend = ({ text }) => {
-	return <p>{text}</p>;
+	return <p className="text-lg italic font-semibold text-gray-200 dark:text-white">{text}</p>;
 };
 
 export default Recommend;
