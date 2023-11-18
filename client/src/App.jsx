a5146682b4d8505a09f54ac6214c5341677e80b5
Greg Pappas <gnpappas@proton.me>
2023-11-18 06:33:22 -1000

    Add new song entry with submit, triggers fetchSongs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,39 @@
 import Form from "./Form";
 import Spotify from "./Spotify";
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-	const formSubmission = (e) => {
+	const [songs, setSongs] = useState([]);
+	const formSubmission = async (e) => {
 		e.preventDefault();
-		console.log(e.target[0].value); //spotify url
-		console.log(e.target[1].value); //recommend text
-		//send db info
+		let url = e.target[0].value;
+		let text = e.target[1].value;
+		await axios.post("api/songs", { url, text });
+		url = "";
+		text = "";
 	};
+
+	const fetchSongs = async () => {
+		try {
+			const response = await axios.get("/api/songs");
+			setSongs(response.data);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	useEffect(() => {
+		fetchSongs();
+	}, [formSubmission]);
+
 	return (
 		<div className="min-h-screen bg-slate-900">
 			<p className="bg-slate-700 text-slate-200 text-5xl text-center p-5">
 				Songification
 			</p>
-			<Form formSubmission={formSubmission} />
-			<Spotify />
+			<Form onClickSubmission={formSubmission} />
+			<Spotify songs={songs} />
 		</div>
 	);
 }
