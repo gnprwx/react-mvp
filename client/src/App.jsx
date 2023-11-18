823b004564c09ee9ab00d79cf5b292b30fdf611c
Greg Pappas <gnpappas@proton.me>
2023-11-17 17:57:47 -1000

    Set text content targets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,20 @@ import Spotify from "./Spotify";
 import { useState } from "react";
 
 function App() {
-	const formSubmission = () => {
+	const formSubmission = (e) => {
+		e.preventDefault();
+		console.log(e.target[0].value); //spotify url
+		console.log(e.target[1].value); //recommend text
 		//send db info
 	};
 	return (
-		<>
+		<div className="min-h-screen bg-slate-900">
 			<p className="bg-slate-700 text-slate-200 text-5xl text-center p-5">
 				Songification
 			</p>
-			<Form formSubmission={formSubmission}/>
-			<Spotify/>
-		</>
+			<Form formSubmission={formSubmission} />
+			<Spotify />
+		</div>
 	);
 }
 
