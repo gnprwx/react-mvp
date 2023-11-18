4ff3b302705138bafa57671fd170825f4641c826
Greg Pappas <gnpappas@proton.me>
2023-11-18 09:35:29 -1000

    Prevent duplicate songs from being submitted

--- server/migration.sql
+++ server/migration.sql
@@ -1,5 +1,6 @@
-CREATE TABLE "songs"(
-    "id" SERIAL PRIMARY KEY,
-    "url" TEXT NOT NULL,
-    "text" TEXT NOT NULL
+DROP TABLE IF EXISTS songs;
+CREATE TABLE songs (
+  "id" SERIAL PRIMARY KEY,
+  "url" VARCHAR(255) UNIQUE,
+  "text" TEXT
 );
