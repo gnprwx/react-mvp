f58f303b920a5b746e6ef7951756e4bf85b1e9b2
Greg Pappas <gnpappas@proton.me>
2023-11-17 20:55:16 -1000

    Set Spotify component to fetch from DB

--- client/src/Spotify.jsx
+++ client/src/Spotify.jsx
@@ -1,26 +1,29 @@
+import axios from "axios";
 import Widget from "./Widget";
 import Recommend from "./Recommend";
+import { useCallback, useEffect, useState } from "react";
 const Spotify = () => {
-	const submission = [
-		{
-			id: 1,
-			url: "6hHhQ8903wgsSovUGjZbGd",
-			text: "This song is amazing",
-		},
-		{
-			id: 2,
-			url: "0Fkf1np3twDAV9JxkYO1ql",
-			text: "This song is sick",
-		},
-	];
+	const [songs, setSongs] = useState([]);
+
+	const fetchSongs = useCallback(async () => {
+		try {
+			const response = await axios.get("/api/songs");
+			setSongs(response.data);
+		} catch (err) {
+			console.error(err);
+		}
+	});
+	useEffect(() => {
+		fetchSongs();
+	}, [fetchSongs]);
+
 	//state uri submission needs to be passed down to here to utilize for id attribute
 	return (
 		<div className="grid grid-cols-3 gap-4 p-5">
-			{submission.map((song) => {
+			{songs.map((song) => {
 				return (
-					<div>
-						<Widget id={song.id} songID={song.url} />{" "}
-						<Recommend text={song.text} />
+					<div key={song.id}>
+						<Widget songID={song.url} /> <Recommend text={song.text} />
 					</div>
 				);
 			})}
