3bc01f9f17f298f4b8072bb2961198b03c0a5fa5
Greg Pappas <gnpappas@proton.me>
2023-11-18 08:29:26 -1000

    Make REST compliant

--- server/server.js
+++ server/server.js
@@ -9,17 +9,28 @@ await client.connect();
 
 app.use(express.json());
 
-app.get("/api/songs", (req, res) => {
-	client.query("SELECT * FROM songs").then((data) => res.send(data.rows));
+app.get("/api/songs", (_, res, next) => {
+	client
+		.query("SELECT * FROM songs ORDER BY id DESC")
+		.then((data) => res.send(data.rows))
+		.catch(next);
 });
 
-app.post("/api/songs", (req, res) => {
+app.post("/api/songs", (req, res, next) => {
 	const { url, text } = req.body;
-	client.query(`INSERT INTO songs(url,text) VALUES($1, $2) RETURNING *`, [
-		url,
-		text,
-	]);
+	client
+		.query(`INSERT INTO songs(url,text) VALUES($1, $2)`, [url, text])
+		.catch(next);
 	res.sendStatus(201);
+});
+
+app.use((_, res) => {
+	res.sendStatus(404);
+});
+
+app.use((err, req, res, next) => {
+	console.log(err);
+	res.sendStatus(500);
 });
 
 app.listen(PORT, () => {
