adcd97c35c80af9bfc82fb6c3622ae8ab0c54d15
Greg Pappas <gnpappas@proton.me>
2023-11-18 19:18:01 -1000

    Styling

--- client/src/Spotify.jsx
+++ client/src/Spotify.jsx
@@ -2,7 +2,9 @@ import Widget from "./Widget";
 import Recommend from "./Recommend";
 const Spotify = ({ songs }) => {
 	return (
-		<div className="grid grid-cols-1 sm:grid-cols-2 gap-10 py-5 px-10">
+		<div
+			className="grid grid-cols-1 sm:grid-cols-2 gap-10"
+		>
 			{songs.map((song) => {
 				return (
 					<div key={song.id} className="drop-shadow-xl">
