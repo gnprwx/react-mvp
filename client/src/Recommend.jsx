a117f0e7ed0e44d82337195e7889040303f6111f
Greg Pappas <gnpappas@proton.me>
2023-11-20 06:49:02 -1000

    Formatting

--- client/src/Recommend.jsx
+++ client/src/Recommend.jsx
@@ -1,5 +1,9 @@
 const Recommend = ({ text }) => {
-	return <p className="text-lg italic font-semibold text-gray-200 dark:text-white">{text}</p>;
+	return (
+		<p className="text-lg italic font-semibold text-gray-200 dark:text-white">
+			{text}
+		</p>
+	);
 };
 
 export default Recommend;
