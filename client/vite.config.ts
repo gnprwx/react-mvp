a9438d05c262b5d65ea96abf3261a3262872c3da
Greg Pappas <gnpappas@proton.me>
2023-11-21 05:39:39 -1000

    Comment out proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,9 +5,9 @@ dotenv.config({ path: "../.env" });
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": `http://localhost:${process.env.PORT}`,
-		},
-	},
+	// server: {
+	// 	proxy: {
+	// 		"/api": `http://localhost:${process.env.PORT}`,
+	// 	},
+	// },
 });
